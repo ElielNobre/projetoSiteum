388638dac34aae82ea19b8c8c1af572421d2397f
DevnoobEli <62188003+DevnoobEli@users.noreply.github.com>
2020-05-07 22:19:09 -0300

    up

--- projeto.php
+++ projeto.php
@@ -1,4 +1,10 @@
 <?PHP
 $nome=$_GET["tnome"];
-echo "<h3>O nome citado foi $nome."; 
-?>
+$data=$_GET["datanascimento"];
+$sexo=$_GET["tsexo"];
+echo "<h3>Nome: $nome.</h3>"; 
+echo "<h3>A data de nascimento:$data</h3>";
+echo "<h3>Sexo: $sexo.</h3>";
+echo "<h1>Cadastrado com sucesso</h1>"?>
+
+<h1><a href="index.php">Voltar</a></h1>
