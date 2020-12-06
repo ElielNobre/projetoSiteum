3c9fa5b0ef3003c517013ee3dd9d9a68ce09a10a
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-05 21:14:19 -0300

    nova pagina
    Aula 11 inserida, aula de condições.

---
+++ js/js.js
@@ -0,0 +1,10 @@
+var velatual = 120;
+var limite = 100;
+
+console.log(`A velocidade de seu carro é de ${velatual} por hora`);
+
+if (velatual >= limite){
+    console.log('O seu carro vai ser mutado por execesso de velocidade')
+}else{
+    console.log('Dirija com cuidado!')
+};
