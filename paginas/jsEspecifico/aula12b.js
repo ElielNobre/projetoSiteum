2efaa2abc37589642424adb7139614e2d06cfc5f
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-09 18:02:41 -0300

    novo exercicio

--- paginas/jsEspecifico/aula12b.js
+++ paginas/jsEspecifico/aula12b.js
@@ -10,31 +10,46 @@ var sex = String(sexo.value)
 
 var mensagem =window.document.getElementById('mes')
 
+var game = window.document.getElementById('jogo');
+var jogo = String(game.value)
+
+var ilustracao = window.document.getElementById('il');
 
 if (id <= 12 && sex === 'Feminino'){
-    imagem.src = '../imagens/menina.png' 
+    imagem.src = '../imagens/aula12b/menina.png'; 
 }
 else if(id <= 12 && sex === 'Masculino'){
-    imagem.src = '../imagens/menino.png'
+    imagem.src = '../imagens/aula12b/menino.png'
 }
 else if(id > 12 && id <= 18  && sex === 'Feminino'){
-    imagem.src = '../imagens/adolecentef.png'
+    imagem.src = '../imagens/aula12b/adolecentef.png'
 }
 else if(id > 12 && id <= 18 && sex === 'Masculino'){
-    imagem.src = '../imagens/adolecentem.png'
+    imagem.src = '../imagens/aula12b/adolecentem.png'
 }
 else if(id > 18 && id <= 50 && sex === 'Feminino'){
-    imagem.src = '../imagens/mulher.png'
+    imagem.src = '../imagens/aula12b/mulher.png'
 }
 else if(id > 18 && id <= 50 && sex === 'Masculino'){
-    imagem.src = '../imagens/homem.png'
+    imagem.src = '../imagens/aula12b/homem.png'
 }
 else if(id > 50 && sex === 'Feminino'){
-    imagem.src = '../imagens/idosa.png'
+    imagem.src = '../imagens/aula12b/idosa.png'
 }
 else if(id > 50 && sex === 'Masculino'){
-    imagem.src = '../imagens/idoso.png'
+    imagem.src = '../imagens/aula12b/idoso.png'
 }
 
 mensagem.innerHTML = `<p>Nome: ${name}, do sexo ${sex} e com ${id} anos;</p>`;
+
+    if(jogo==='fortnite'){
+            ilustracao.src = '../imagens/aula12a/bomdia.png';
+    }else if(jogo === 'free'){
+        ilustracao.src = '../imagens/aula12a/bomdia.png';
+    }else if (jogo === 'fg'){
+        ilustracao.src = '../imagens/aula12a/bomdia.png';
+    }else if (jogo === 'mc'){
+        ilustracao.src = '../imagens/aula12a/bomdia.png';
+    }
+
 }
