f85063d65447c12f65d9f8ec91e4b802f7c03bb4
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-12 20:04:22 -0300

    exercio13e14
    repetição com for

--- estudo/paginas/jsEspecifico/aula13.js
+++ estudo/paginas/jsEspecifico/aula13.js
@@ -1,11 +1,22 @@
 function contar() {        
-   let inicio = document.getElementById('txti')
-   let fim = document.getElementById('txtf')
-   let passo = document.getElementById('txtp')
-     
+   var inicio = document.getElementById('txti')
+   var fim = document.getElementById('txtf')
+   var passo = document.getElementById('txtp')
+   var res = window.document.getElementById('res')
+
+  
+  
    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados')     
+        res.innerHTML= '[ERRO] Faltam dados'     
    }else{
-       window.alert('Tudo certo')
+        res.innerHTML = 'Começou...<br/>'
+        let begin = Number(inicio.value)
+        let end = Number(fim.value)
+        let pass = Number(passo.value)
+
+        for (let i = begin;i <= end;i += pass){
+          res.innerHTML += `&#128073;&#127995;${i}<br/>`
+        }
+        res.innerHTML += '<h1>&#9997;&#127997;</h1>'
    }
  }
