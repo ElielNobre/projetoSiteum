3b913a43740b1d8d4c2340545e97f4b9285a64b5
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-11 22:16:17 -0300

    mais paginas e imagens

--- estudo/js/js.js
+++ estudo/js/js.js
@@ -1,4 +1,4 @@
-var velatual = 120;
+/*var velatual = 120;
 var limite = 100;
 
 console.log(`A velocidade de seu carro é de ${velatual} por hora`);
@@ -7,4 +7,10 @@ if (velatual >= limite){
     console.log('O seu carro vai ser mutado por execesso de velocidade')
 }else{
     console.log('Dirija com cuidado!')
-};
+};*/
+console.log('Vai Começar')
+for (var c =1; c<= 10; c++){
+    console.log(`Eu sou ${c}`)
+}
+console.log('FIM')
+
