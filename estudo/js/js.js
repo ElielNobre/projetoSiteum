fe36755a3d53dcbc07c5a747becaabe9499092f2
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-13 16:22:46 -0300

    estudos avançados

--- estudo/js/js.js
+++ estudo/js/js.js
@@ -7,10 +7,27 @@ if (velatual >= limite){
     console.log('O seu carro vai ser mutado por execesso de velocidade')
 }else{
     console.log('Dirija com cuidado!')
-};*/
+};
 console.log('Vai Começar')
 for (var c =1; c<= 10; c++){
     console.log(`Eu sou ${c}`)
 }
-console.log('FIM')
+console.log('FIM')*/
+
+/*let num = [1,2,3,4,5,6,7,8]
+num.push(9)
+num.sort()
+console.log(num.sort())
+console.log(num.length)
+console.log(num[2])*/
+
+let num =[1,5,9,2,4,1,2,1,1,2,4,56,67,4,3,2,4,65,7,5,3,2,4556]
+
+for (let i = 0;i <= num.length-1; i++){
+    console.log(num[i]);
+}
+
+
+
+
 
