c249c91c541714b55a73ef5ad6f09e9dfc5e20bd
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-20 19:36:14 -0300

    js projeto

--- estudo/js/js.js
+++ estudo/js/js.js
@@ -77,7 +77,7 @@ let i = numeros.length - 1;
 do {
   console.log(`${numeros.reverse()}`)
   i--;
-} while(numeros < i);*/
+} while(numeros < i);*//*
 const tags = [ "Python", "C++", "F#" ];
 
 if (tags.length >= 3) {
@@ -86,5 +86,32 @@ if (tags.length >= 3) {
   }
 } else {
   console.log("Insira pelo menos 3 tags");
-}
+}*/
+var a = 3;
+var b = -7;
+var c = 4;
+
+var delta = (Math.pow(b,2))-(4*a*c)
+
+console.log(delta)
+    if (delta>=0){
+        var x1 =((b*(-1))+Math.sqrt(delta))/(2*a)
+        var x2 =((b*(-1))-Math.sqrt(delta))/(2*a)
+         
+        var resul1 = (a*(Math.pow(x1,2)))+(b*x1)+c
+        var resul2 = (a*(Math.pow(x2,2)))+(b*x2)+c
+        
+        console.log(`valores de x1: ${x1} e x2: ${x2}`)
+        console.log(`O resultado para X1 é ${resul1} e para X2 é ${resul2}`);
+
+    }else{
+        console.log('Não é um número real por causa do Delta')
+    }
+
+
+
+
+
+
+
 
