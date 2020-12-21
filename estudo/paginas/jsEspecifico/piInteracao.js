f53f6eb3e2292942a09891bfefd7ffbd4541fe95
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-21 00:22:50 -0300

    equacao
    resolução

--- estudo/paginas/jsEspecifico/piInteracao.js
+++ estudo/paginas/jsEspecifico/piInteracao.js
@@ -3,17 +3,29 @@ var num2 = window.document.getElementById('b');
 var num3 = window.document.getElementById('c');
 var resp = window.document.getElementById('resultado');
 
+function calcular(){
 var a= Number(num1.value)
 var b= Number(num2.value)
 var c= Number(num3.value)
 
-function calcular(){
-    resp.innerHTML +=`O valor de a: ${a}<br/>O valor de b: ${b}<br/>O valor de c: ${c}`
+    resp.innerHTML +=`O valor de a: ${a}<br/>O valor de b: ${b}<br/>O valor de c: ${c}<br/>`
+    var delta = (Math.pow(b,2))-(4*a*c)
+    resp.innerHTML += `O valor de Delta é: ${delta}<br/>`
+
+    if (delta >= 0){
+        var x1 = ((b*(-1))+(Math.sqrt(delta)))/(2*a)
+        var x2 = ((b*(-1))-(Math.sqrt(delta)))/(2*a)
+        let xx1 = parseFloat(x1)
+        let xx2 = parseFloat(x2)
+        resp.innerHTML += `O valor de x1: ${xx1}<br/>`
+        resp.innerHTML += `O valor de x2: ${xx2}<br/>`
+    }
+
 }
      /*`O valor de a: ${a}<br/>O valor de b: ${b}<br/>O valor de c: ${c}`;*/
 
 /*
-var delta = (Math.pow(b,2))-(4*a*c)
+
 
 console.log(delta)
     if (delta>=0){
