75b2f8cbc0a5b895a6202d41a9eec90c2c6e0d03
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-24 15:33:46 -0300

    canvas geometria

--- estudo/paginas/jsEspecifico/geometria.js
+++ estudo/paginas/jsEspecifico/geometria.js
@@ -1,12 +1,24 @@
 function desenhar() {
     var tela = document.getElementById("canvas");
+    var mensagem = document.getElementById("mensagem")
+    var cat1 = document.getElementById("ct1");
+    var cat2 = document.getElementById("ct2");
+    
     if (tela.getContext) {
       var contexto = tela.getContext("2d");
+      let c1 = Number(cat1.value)
+      let c2 = Number(cat2.value)
+      let quad = c1 * c2
+      let tria = (c1*c2)/2
+      let hipo = Math.pow(Math.pow(c1,2)+ Math.pow(c2,2))
+        let hp = parseFloat(hipo);
 
       contexto.fillStyle = "rgb(200,0,0)";
-      contexto.fillRect (10, 10, 55, 50);
-
-      contexto.fillStyle = "rgba(0, 0, 200, 0.5)";
-      contexto.fillRect (30, 30, 55, 50);
+      contexto.fillRect(40, 40, c1, c2);
+      mensagem.innerHTML = `A área do quadrado é: ${quad} pixels.<br/>`
+      mensagem.innerHTML += `A área do quadrado é: ${tria} pixels.<br/>`
+      mensagem.innerHTML += `O valor da hipotenusa ou da diagonal do retangulo é: ${hp} pixels.`
+     /* contexto.fillStyle = "rgba(0, 0, 200, 0.5)";
+      contexto.fillRect (30, 30, 55, 50);*/
     }
   }
