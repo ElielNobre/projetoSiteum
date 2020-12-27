c9854eb39bafb0f551aa2e16ad8977999c2b3d41
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-26 23:08:41 -0300

    alteração
    novas variaveis

--- estudo/paginas/jsEspecifico/circulo.js
+++ estudo/paginas/jsEspecifico/circulo.js
@@ -2,13 +2,13 @@ var telac = document.getElementById("canvas2");
 var contexto = telac.getContext("2d")        
 var raio = document.getElementById("raio"); 
 var resposta = document.getElementById("respcir")  
-var area = Math.pow(raio,2)*Math.PI;
 
 function desenharcirculo(){
     if(telac.getContext){
-            var r = Number(raio.value);
-            var area = Math.pow(r,2)*Math.PI;
-            contexto.arc(150,80,r,0,Math.PI*2)
+            let r = Number(raio.value);
+            let p = Math.PI
+            let area = Math.pow(r,2)*p;
+            contexto.arc(150,80,r,0, p*2)
             contexto.fill();
             resposta.innerHTML = `A area do círculo é: ${area.toFixed(2)}pixels quadrados.`
     }
