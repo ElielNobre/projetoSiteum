6025ec7944ca3f5557c4b0488a04150f8784fd14
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-27 03:18:20 -0300

    canvas
    beginPath e closePath nunca mais esquecerei

--- estudo/paginas/jsEspecifico/circulo.js
+++ estudo/paginas/jsEspecifico/circulo.js
@@ -1,22 +1,29 @@
 var telac = document.getElementById("circulo"); 
+let resposta = document.getElementById("respcir");  
+let contexto = telac.getContext("2d");
 let raio = document.getElementById("raio");   
-let resposta = document.getElementById("respcir")  
-let contexto = telac.getContext("2d")
+const p =Math.PI 
 
-function desenharcirculo(){         
-            let r = Number(raio.value); 
-            const p = Math.PI 
-            let area = Math.pow(r,2)*p;
-            contexto.fillStyle ='green';
-            contexto.arc(150,80,r,0, p*2);
-            contexto.fill();
-            resposta.innerHTML = `A area do círculo é: ${area.toFixed(2)}pixels quadrados.`
+function desenharcirculo(){ 
+        let r = Number(raio.value);  
+        let area = Math.pow(r,2)*p;
+        contexto.beginPath();
+        contexto.fillStyle ='green';
+        contexto.arc(150,80,r,0, p*2);
+        contexto.closePath();
+        contexto.fill();
+        resposta.innerHTML = `A area do círculo é: ${area.toFixed(2)}pixels quadrados.`
 }
 
 function limparcirculo(){     
-    contexto.clearRect(0,0,2000,2000);
+    contexto.clearRect(0,0,telac.width,telac.height);
     /*desenharcirculo() = null;*/
-    window.location.reload();
+    /*window.location.reload();*/
     /*não é o ideal mas é só pra ter o prazer de dizer 
     que não perdi essa batalha;*/ 
 }
+/*function meupau(){
+    if(raio = 0){
+    raio = document.getElementById("raio"); )
+    }
+    }*/
