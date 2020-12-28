75cf34a676f44f88844b3451950a7810ddb38d1d
Eliel Nóbrega <62188003+ElielNobre@users.noreply.github.com>
2020-12-28 02:10:23 -0300

    SPA

--- estudo/paginas/jsEspecifico/circulo.js
+++ estudo/paginas/jsEspecifico/circulo.js
@@ -1,14 +1,13 @@
 var telac = document.getElementById("circulo"); 
-let resposta = document.getElementById("respcir");  
+let res = document.getElementById("respcir");  
 let contexto = telac.getContext("2d");
 let raio = document.getElementById("raio");   
-const p =Math.PI 
+var p = Math.PI 
 
 function desenharcirculo(){ 
         let r = Number(raio.value);  
         let area = Math.pow(r,2)*p;
         contexto.beginPath();
-        contexto.fillStyle ='green';
         contexto.arc(150,80,r,0, p*2);
         contexto.closePath();
         contexto.fill();
@@ -17,13 +16,4 @@ function desenharcirculo(){
 
 function limparcirculo(){     
     contexto.clearRect(0,0,telac.width,telac.height);
-    /*desenharcirculo() = null;*/
-    /*window.location.reload();*/
-    /*não é o ideal mas é só pra ter o prazer de dizer 
-    que não perdi essa batalha;*/ 
 }
-/*function meupau(){
-    if(raio = 0){
-    raio = document.getElementById("raio"); )
-    }
-    }*/
--- estudo/paginas/jsEspecifico/retaquad.js
+++ estudo/paginas/jsEspecifico/retaquad.js
@@ -8,7 +8,6 @@ function desenhar() {
       let c1 = Number(cat1.value)
       let c2 = Number(cat2.value)
       let quad = c1 * c2
-      let tria = (c1*c2)/2
         let hipo = Math.sqrt(Math.pow(c1,2)+ Math.pow(c2,2))
         let hp = parseFloat(hipo.toFixed(2));
     
@@ -16,7 +15,6 @@ function desenhar() {
       contexto.fillRect(20, 20, c1, c2);
 
       mensagem.innerHTML = `A área do quadrilátero é: ${quad} pixels.<br/>`
-      mensagem.innerHTML += `A área do triangulo-Retangulo é: ${tria} pixels.<br/>`
       mensagem.innerHTML += `O valor da hipotenusa ou da diagonal do quadrilátero é: ${hp} pixels.`
   }
 function limpartela(){
@@ -24,4 +22,24 @@ function limpartela(){
   contexto.clearRect(0,0,2000,2000);
 }
 
+var telat = document.getElementById("triangulo");  
+var ctxt = telat.getContext("2d");
+var cateto1 = document.getElementById("cateto1");
+var cateto2 = document.getElementById("cateto2");
+var resposta = document.getElementById("resptri");
 
+function desenhartri(){
+  let a = Number(cateto1.value);
+  let b = Number(cateto2.value);
+  let area = (a * b)/2;
+ 
+    ctxt.beginPath();
+    ctxt.moveTo(200,30);
+    ctxt.lineTo(200,100);
+    ctxt.lineTo(70,100);
+    ctxt.fill();
+  resposta.innerHTML = `A area do triangulo é:${area} pixels quadrados.`
+}
+function limpartri(){
+  ctxt.clearRect(0,0,telat.width,telat.height);
+}
